e97d82be361a9491e9acf7071024f1e6ef452eda
dark-ether <henrique.hsa2002@gmail.com>
2021-12-06 22:38:13 -0300

    added images

---
+++ library/javascript/macros/combat/attack.js
@@ -0,0 +1,7 @@
+const jp = require("jsonpath");
+const _ = require("lodash");
+const basic = require("../../basic.js");
+
+function attack(attackertid,defendertid,injury,bodyPart="torso"){
+    basic.callOnOwner(defendertid,)
+}
