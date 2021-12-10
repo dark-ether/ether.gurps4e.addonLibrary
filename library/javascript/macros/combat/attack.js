3576940d8dccb6a668700f2325d34ad7a6b75cc0
dark-ether <henrique.hsa2002@gmail.com>
2021-12-09 21:50:37 -0300

    minor bugfix in attack

--- library/javascript/macros/combat/attack.js
+++ library/javascript/macros/combat/attack.js
@@ -1,5 +1,3 @@
-const jp = require("jsonpath");
-const _ = require("lodash");
 const basic = require("../../basic.js");
 
 function attack(attackertid,defendertid,injury,bodyPart="torso",specialEffects={}){
---
+++ library/mtscript/public/defense/exampleAttack.mts
@@ -0,0 +1,5 @@
+[r:dice = 1d6]
+[r:skillRoll = 3d6]
+[r:damageType = "impaling"]
+[r:broadcast("rolled : "+skillRoll+" damage: "+dice)]
+[r:"placeholder"]
